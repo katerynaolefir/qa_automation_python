fccb7b9c6b69dcfacd8fd7e7ced1f3591f2043e3
Kateryna <katery.olefir@gmail.com>
2026-08-14 17:38:34 +0300

    Fix in homework_9.1.py

--- lesson_09/homework_9.1.py
+++ lesson_09/homework_9.1.py
@@ -6,20 +6,23 @@ class Rhombus:
     def __setattr__(self, key, value):
         if key == 'side_a':
             if not isinstance(value, (int, float)):
-                raise TypeError('Сторона повинна бути числом')
+                raise TypeError('Сторона повинна бути числом.')
             if value > 0:
                 super().__setattr__(key, value)
             else:
-                raise ValueError('Сторона повинна бути більше 0')
+                raise ValueError('Сторона повинна бути більше 0.')
 
         elif key == 'angle_a':
             if not isinstance(value, (int, float)):
-                raise TypeError('Кут повинен бути числом')
+                raise TypeError('Кут повинен бути числом.')
             if 0 < value < 180:
                 super().__setattr__(key, value)
                 super().__setattr__('angle_b', 180 - value)
             else:
-                raise ValueError('Кут повинен бути від 0 до 180')
+                raise ValueError('Кут повинен бути від 0 до 180.')
+        elif key == 'angle_b':
+            print('Кут Б обчислюється автоматично.')
+            return
 
         else:
             super().__setattr__(key, value)
@@ -27,4 +30,5 @@ class Rhombus:
 
 
 figure = Rhombus(5, 60)
-print(f"Сторона ромба: {figure.side_a}, Кут А: {figure.angle_a}, Кут Б: {figure.angle_b}")
+figure.angle_b = 20
+print(f"Сторона ромба: {figure.side_a}, Кут А: {figure.angle_a}, Кут Б: {figure.angle_b}.")

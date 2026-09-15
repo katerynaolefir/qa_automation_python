d40e44d770e999be7e6c04db17c7b6fa4df2b3b3
Kateryna <katery.olefir@gmail.com>
2026-09-15 11:43:38 +0300

    Fix files in lesson_16

---
+++ lesson_16/randominfo/randominfo-master/test.py
@@ -0,0 +1,17 @@
+from randominfo import Person
+person = Person()
+print(
+    f"Id: {person.id}\n"
+    f"Password: {person.paswd}\n"
+    f"Full Name: {person.full_name}\n"
+    f"Birthdate: {person.birthdate}\n"
+    f"Gender: {person.gender}\n"
+    f"Email: {person.email}\n"
+    f"Phone: {person.phone}\n"
+    f"Hobbies: {person.hobbies}\n"
+    f"Country: {person.country}\n"
+    f"Address: {person.address}\n"
+    f"Other Attributes: {person.customAttr}"
+
+)
+
